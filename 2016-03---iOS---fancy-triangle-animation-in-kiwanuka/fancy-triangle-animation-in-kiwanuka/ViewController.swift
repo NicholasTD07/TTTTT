460879157687313c61e0f0b3a44fb99b83526d17
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-03-12 23:02:41 +1100

    Draw a boring red triangle's border

--- 2016-03---iOS---fancy-triangle-animation-in-kiwanuka/fancy-triangle-animation-in-kiwanuka/ViewController.swift
+++ 2016-03---iOS---fancy-triangle-animation-in-kiwanuka/fancy-triangle-animation-in-kiwanuka/ViewController.swift
@@ -12,6 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        drawTriangle()
+    }
+
+    func drawTriangle() {
+        let angle = M_PI/3
+        let center = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        let radius = 100 as Double
+
+        let xOffset = CGFloat(sin(angle) * radius)
+        let yOffset = CGFloat(cos(angle) * radius)
+
+        let top = CGPoint(x: center.x, y: center.y - CGFloat(radius))
+        let left = CGPoint(x: center.x - xOffset, y: center.y + yOffset)
+        let right = CGPoint(x: center.x + xOffset, y: center.y + yOffset)
+
+        let path = UIBezierPath()
+        path.moveToPoint(top)
+        path.addLineToPoint(left)
+        path.addLineToPoint(right)
+        path.addLineToPoint(top)
+        path.closePath()
+
+        let triangle = CAShapeLayer()
+        triangle.path = path.CGPath
+        triangle.fillColor = nil
+        triangle.strokeColor = UIColor.redColor().CGColor
+
+        view.layer.addSublayer(triangle)
     }
 }
 
