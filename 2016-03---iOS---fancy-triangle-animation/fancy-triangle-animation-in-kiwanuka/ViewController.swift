73ed57f62a4db4b53388c380ece1f9572384ad83
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-03-13 09:29:12 +1100

    [fancy-triangle-animation] Extract path creation of equilateral triangle
    You can animate the path of a CAShapeLayer. With this extraction, you
    can use the extracted method to get 2 triangles at the same center but
    with different radius to animate the size change.

--- 2016-03---iOS---fancy-triangle-animation/fancy-triangle-animation-in-kiwanuka/ViewController.swift
+++ 2016-03---iOS---fancy-triangle-animation/fancy-triangle-animation-in-kiwanuka/ViewController.swift
@@ -16,9 +16,25 @@ class ViewController: UIViewController {
     }
 
     func drawTriangle() {
-        let angle = M_PI/3
         let center = CGPoint(x: view.frame.midX, y: view.frame.midY)
         let radius = 100 as Double
+
+        let path = EquilateralTriangle.pathForTriangle(centeredAt: center, radius: radius)
+
+        let triangle = CAShapeLayer()
+        triangle.path = path.CGPath
+        triangle.fillColor = nil
+        triangle.strokeColor = UIColor.redColor().CGColor
+
+        view.layer.addSublayer(triangle)
+    }
+}
+
+struct EquilateralTriangle {
+    static let angle = M_PI/3
+
+    static func pathForTriangle(centeredAt center: CGPoint, radius: Double = 10) -> UIBezierPath {
+        let path = UIBezierPath()
 
         let xOffset = CGFloat(sin(angle) * radius)
         let yOffset = CGFloat(cos(angle) * radius)
@@ -27,19 +43,12 @@ class ViewController: UIViewController {
         let left = CGPoint(x: center.x - xOffset, y: center.y + yOffset)
         let right = CGPoint(x: center.x + xOffset, y: center.y + yOffset)
 
-        let path = UIBezierPath()
         path.moveToPoint(top)
         path.addLineToPoint(left)
         path.addLineToPoint(right)
-        path.addLineToPoint(top)
         path.closePath()
 
-        let triangle = CAShapeLayer()
-        triangle.path = path.CGPath
-        triangle.fillColor = nil
-        triangle.strokeColor = UIColor.redColor().CGColor
-
-        view.layer.addSublayer(triangle)
+        return path
     }
 }
 
