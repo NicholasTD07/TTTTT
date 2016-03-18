9bb1ed8a89ad389a7aa730a7be8f22d6abcb5d04
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-03-18 11:08:34 +1100

    [project-naming-helper] Add usage when no args

--- t.py
+++ t.py
@@ -6,6 +6,10 @@ import os
 
 args = sys.argv[1:]
 
+if len(args) < 2:
+    print("Usage: {} path tag1 tag2\n\n{} will rename path to 'Year-Month---tag1-tag2---path'.".format(sys.argv[0], sys.argv[0]))
+    exit(1)
+
 path, tags = args[0], args[1:]
 
 path_exists = os.path.exists(path)
