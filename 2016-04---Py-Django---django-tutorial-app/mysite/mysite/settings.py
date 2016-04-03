986476a8c09e1ca48d7eb55b1acc9cf97e108bbb
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-04-03 19:58:54 +1000

    [django-tutorial-app] Add models: Question and Choice

--- 2016-04---Py-Django---django-tutorial-app/mysite/mysite/settings.py
+++ 2016-04---Py-Django---django-tutorial-app/mysite/mysite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+django_apps = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+apps = [
+        'polls.apps.PollsConfig',
+]
+
+INSTALLED_APPS = django_apps + apps
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
