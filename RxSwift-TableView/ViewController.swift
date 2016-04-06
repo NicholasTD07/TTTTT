0062f3877527313d05f59a2a640fcfc5cc86657e
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-04-06 17:52:45 +1000

    [RxSwift-TableView] init project with RxSwift and RxCocoa pods

---
+++ RxSwift-TableView/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RxSwift-TableView
+//
+//  Created by Nicholas Tian on 6/04/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
