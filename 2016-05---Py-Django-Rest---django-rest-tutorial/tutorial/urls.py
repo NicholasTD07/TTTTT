a634350317439dbcbab34aba96528512551e960f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-05-09 19:43:01 +1000

    [django-rest-tutorial] Adding views and urls

--- 2016-05---Py-Django-Rest---django-rest-tutorial/tutorial/urls.py
+++ 2016-05---Py-Django-Rest---django-rest-tutorial/tutorial/urls.py
@@ -1,14 +1,25 @@
 from django.conf.urls import url, include
-from rest_framework import routers
-from tutorial.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+from tutorial.quickstart import views as q_views
+from snippets import views
+
+# router = routers.DefaultRouter()
+# router.register(r'users', q_views.UserViewSet)
+# router.register(r'groups', q_views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^', include(router.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
