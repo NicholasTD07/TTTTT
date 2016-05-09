fe943bf937c63de9bb4bb80062e8e3ee2d30e992
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-05-09 20:10:14 +1000

    [django-rest-take-2] Add 3rd party and user apps in settings.py

--- 2016-05---Py-Django-Rest---django-rest-take-2/tutorial/settings.py
+++ 2016-05---Py-Django-Rest---django-rest-take-2/tutorial/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+USER_APPS = [
+    'snippets.apps.SnippetsConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + USER_APPS
+
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
