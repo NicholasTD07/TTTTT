bbd113c7321e5ab5b7cac8fa26e789c4edadab4f
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-05-10 19:44:02 +1000

    [django-rest-take-2] Add User to serializers, urls, views

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
