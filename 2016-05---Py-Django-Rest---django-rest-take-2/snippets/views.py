ed57f9513aeb3efb375295e400295c14f6a02d90
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-05-10 21:04:18 +1000

    [django-rest-take-2] Remove api_root because DefaultRouter does it for us

--- 2016-05---Py-Django-Rest---django-rest-take-2/snippets/views.py
+++ 2016-05---Py-Django-Rest---django-rest-take-2/snippets/views.py
@@ -10,14 +10,6 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `detail` actions.
