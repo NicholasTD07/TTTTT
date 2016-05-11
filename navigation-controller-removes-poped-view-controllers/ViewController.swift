d2b520208bafd1a33f92df721b132114821ba97c
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-05-11 21:04:36 +1000

    [ios-double-navigation]New iOS proof of concept project

---
+++ navigation-controller-removes-poped-view-controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigation-controller-removes-poped-view-controllers
+//
+//  Created by Nicholas Tian on 11/05/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
