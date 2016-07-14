5ae5e739f7caa502c7a6dfd83fbe0372b145d765
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-07-14 11:39:19 +1000

    [swift] You can modify arrays in dict with append 😝

--- swift-experiments/array-in-dict.swift
+++ swift-experiments/array-in-dict.swift
@@ -1,21 +1,37 @@
 /*
  Conclusion: `Dictionary` and `Array` in Swift are value types
     * If you want to modify an array in a dict
-        * You have to get that array as a `var`
-        * Modify the array
-        * Assign the array to the `dict[key]`
+        * you can do `dict[key]?.append(item)`
 */
 
-var dict = [Int: [Int]]()
+func versionOne() {
+    var dict = [Int: [Int]]()
 
-print(dict[0]) // nil
+    print(dict[0]) // nil
 
-dict[0] = [1]
-print(dict[0]) // Optional([1])
+    dict[0] = [1]
+    print(dict[0]) // Optional([1])
 
-var array = dict[0]
-array?.append(2)
-print(dict[0]) // Optional([1])
+    var array = dict[0]
+    array?.append(2)
+    print(dict[0]) // Optional([1])
 
-dict[0] = array
-print(dict[0]) // Optional([1, 2])
+    dict[0] = array
+    print(dict[0]) // Optional([1, 2])
+}
+
+// versionOne()
+
+func versionTwo() {
+    var dict = [Int: [Int]]()
+
+    print(dict[0]) // nil
+
+    dict[0] = [1]
+    print(dict[0]) // Optional([1])
+
+    dict[0]?.append(2)
+    print(dict[0]) // Optional([1, 2])
+}
+
+versionTwo()
