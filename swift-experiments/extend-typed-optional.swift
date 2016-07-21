c2c59a8421a0c580c5a7cfbd64c5b521e8fb46ad
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-07-21 11:22:22 +1000

    [swift] When extending generics, use a protocol as constraints

---
+++ swift-experiments/extend-typed-optional.swift
@@ -0,0 +1,20 @@
+/*
+ Conclusion: When extending generics, you have to use a prototype as constraints
+*/
+
+// v1 naive
+
+/*
+ error: constrained extension must be declared on the unspecialized generic type 'Optional' with constraints specified by a 'where' clause
+*/
+// extension Optional<String> { }
+
+extension Optional { } // 👍
+
+/*
+ error: same-type requirement makes generic parameter 'Wrapped' non-generic
+*/
+// extension Optional where Wrapped == String { } 
+
+protocol T { }
+extension Optional where Wrapped: T { } // 👍
