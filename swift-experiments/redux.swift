ab8d891fd489b0b85b60dfa89630743ff6145d0c
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-09-05 21:41:13 +1000

    [swift-redux] Store and reducer working together!

--- swift-experiments/redux.swift
+++ swift-experiments/redux.swift
@@ -1,14 +1,27 @@
-protocol StateType { }
-protocol SubscriberType { }
+typealias SubscriberType = (StoreType) -> ()
+
 protocol ActionType { }
+
+protocol StoreType {
+    associatedtype StateType
+    var state: StateType? { get }
+    func subscribe(subscriber: SubscriberType)
+    func dispatch(action: ActionType)
+}
+
+struct InitialAction: ActionType { }
 
 struct ExampleReducers {
     struct Counter {
         typealias State = Int
+        typealias Reducer = (state: State?, action: ActionType) -> State
+
         struct Increase: ActionType { }
         struct Decrease: ActionType { }
 
-        func counter(state: State = 0, action: ActionType) -> State {
+        static func reduceFunc(state: State? = nil, action: ActionType) -> State {
+            let state = state ?? 0
+
             switch action {
             case _ as Increase:
                 return state + 1
@@ -18,12 +31,48 @@ struct ExampleReducers {
                 return state
             }
         }
+
+        class Store: StoreType {
+            typealias StateType = Int
+            var subscribers = [SubscriberType]()
+            var state: StateType?
+            private let reducer: Reducer
+
+            init(reducer: Reducer) {
+                self.reducer = reducer
+
+                dispatch(InitialAction())
+            }
+
+            func dispatch(action: ActionType) {
+                self.state = self.reducer(state: state, action: action)
+                subscribers.forEach {
+                    $0(self)
+                }
+            }
+
+            func subscribe(subscriber: SubscriberType) {
+                subscribers.append(subscriber)
+            }
+        }
     }
 }
 
-protocol Store {
-    var state: StateType { get }
-    func subscribe(subsciber: SubscriberType)
-    func dispatch(action: ActionType)
-}
+let store = ExampleReducers.Counter.Store(
+   reducer: ExampleReducers.Counter.reduceFunc
+)
 
+// initial state is 0
+assert(store.state == 0)
+
+// dispatch unknown actions does not affect the state
+store.dispatch(InitialAction())
+assert(store.state == 0)
+
+// dispatch Increase will increase the state
+store.dispatch(ExampleReducers.Counter.Increase())
+assert(store.state == 1)
+
+// dispatch Decrease will increase the state
+store.dispatch(ExampleReducers.Counter.Decrease())
+assert(store.state == 0)
