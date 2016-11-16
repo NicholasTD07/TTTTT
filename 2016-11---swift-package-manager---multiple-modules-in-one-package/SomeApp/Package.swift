483c28107e2a0b57fb500922a55a15469b54db91
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-11-16 23:01:19 +1100

    [spm] Add all source targets as test target's dependencies
    Fixed the problem: symbol(s) not found

--- 2016-11---swift-package-manager---multiple-modules-in-one-package/SomeApp/Package.swift
+++ 2016-11---swift-package-manager---multiple-modules-in-one-package/SomeApp/Package.swift
@@ -1,5 +1,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SomeApp"
+    name: "SomeApp",
+    targets: [
+        Target(name: "SomeApp", dependencies: []),
+        Target(name: "SomeOtherApp", dependencies: []),
+        Target(name: "SomeAppTests", dependencies: [
+            "SomeApp",
+            "SomeOtherApp",
+        ]),
+    ]
 )
