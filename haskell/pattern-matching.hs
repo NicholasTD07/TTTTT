660d3edc5471e7e3f5267de30c2b2ce944a92ffb
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-02-06 16:06:34 +1100

    [haskell] Add parttern matching

---
+++ haskell/pattern-matching.hs
@@ -0,0 +1,11 @@
+head' :: [a] -> a
+head' [] = error "Cannot call head' on an empty list."
+head' (x:_) = x
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
