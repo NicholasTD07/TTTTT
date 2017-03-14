81f16cd1761df80dea13f041f243864d573f50d8
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-03-14 14:29:45 +1100

    [python] Add experiment to parse swift function signature

---
+++ python/parse-swift-function-signature.py
@@ -0,0 +1,41 @@
+import re
+
+sample = """
+func t() {}
+
+func t123xyz() {}
+
+func t() {
+}
+
+func t() -> T {}
+
+func t(t1: T1) {}
+func t(t1: T1) -> R {}
+func t(t1: T1, t2: T2) -> R {}
+func t(t1: T1, t2: T2, t3 t3: T3) -> R {}
+
+func t(t1: T1,
+       t2: T2,
+       t3 t3: T3) -> R {}
+
+func t(t1: T1,
+       t2: T2,
+       t3 t3: T3
+      ) -> R {}
+
+struct T {
+    func tt()
+}
+"""
+
+
+pattern = r"""
+    func # 
+    \s+ # space between func and name
+    \w+ # func name
+    (
+    [\w\s]+ # args
+    )
+"""
+
