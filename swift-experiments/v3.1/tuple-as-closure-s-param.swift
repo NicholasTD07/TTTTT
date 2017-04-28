87face306b12cbe960f865b93e6e468a64bfdbc7
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-04-28 12:16:58 +1000

    [swift] When use tuple as closure's param, you can also keep it as is

--- swift-experiments/v3.1/tuple-as-closure-s-param.swift
+++ swift-experiments/v3.1/tuple-as-closure-s-param.swift
@@ -1,7 +1,18 @@
 // you do not need extra unpacking to use tuple in closure
 
-typealias T = (name: String, age: Int)
+typealias Person = (name: String, age: Int)
 
-let _: (T) -> Void = { (name, age) in
-
+let t1: (Person) -> Void = { (name, age) in
+    print(name)
+    print(age)
 }
+
+let t2: (Person) -> Void = { t in
+    print(t.name)
+    print(t.age)
+}
+
+let nick: Person = (name: "nick", age: 27)
+
+t1(nick)
+t2(nick)
