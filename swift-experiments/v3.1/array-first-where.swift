ad84ab6463007e68ec7e83db410bf16f5879df2d
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-06-08 14:56:33 +1000

    [swift] PUT param list in the tupleArray.where block!
    tupleArray.first { p1, p2, ... in ... }

--- swift-experiments/v3.1/array-first-where.swift
+++ swift-experiments/v3.1/array-first-where.swift
@@ -1,5 +1,27 @@
 let xs = [1, 2, 3]
 
-print(xs.first(where: { $0 == 1 }))
-print(xs.first(where: { $0 == 4 }))
-print(xs.first { $0 == 4 })
+print(xs.first(where: { $0 == 1 })!)
+print(xs.first { $0 == 4 } as Any)
+
+class T {}
+
+let t = T()
+let ts = [t, T(), T(), T()]
+
+print(ts.filter { $0 === t }.first!)
+print(ts.first { $0 === t }!)
+
+let ys = [
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+]
+
+// error: cannot call value of non-function type '(Int, Int)?'
+// ys.first { true }
+// error: cannot call value of non-function type '(Int, Int)?'
+// ys.first(where: { true } )
+// print(ys.first { _,_ in true }!) // swift 3.1
+print(ys.first(where: { _ in true })!) // swift 3.1 & 4
