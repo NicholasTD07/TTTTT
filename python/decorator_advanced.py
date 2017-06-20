5afdf064cdf3dd2182d36bd7c525c067acfe3fdc
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-06-21 09:55:24 +1000

    [python] Explain 3 level deep decorators

--- python/decorator_advanced.py
+++ python/decorator_advanced.py
@@ -1,7 +1,30 @@
+"""
+Decorators need to be three levels deep, to
+
+- take parameters
+- wrap around functions that take parameters
+- process the return value(s) of the functions,
+
+
+def decorator(params_for_itsef):
+
+    def wrapper_around(a_function):
+
+        def wrapped_function(*args, **kwargs):
+            return_value = a_function(*args, **kwargs):
+            processed = process(return_value)
+
+            return processed
+
+        return wrapped_function
+
+    return wrapper_around
+"""
+
 def add(key, value_func):
     def wrapper(func):
-        def nested(arg, kwarg):
-            y = func(arg, kwarg=kwarg)
+        def nested(*args, **kwargs):
+            y = func(*args, **kwargs)
             y[key] = value_func(y)
             return y
         return nested
